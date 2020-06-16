db280fe37c94871d7a962bfabdec7cd422055a08
laxmanasarvesh <laxmanasarvesh@gmail.com>
2020-06-16 14:00:30 +0530

    adding device drivers exit ,module total 3 files

---
+++ ex03_simple_module_exit.c
@@ -0,0 +1,8 @@
+#include<linux/init.h>
+#include<linux/module.h>
+void ex03_simple_module_exit(void)
+{
+	printk(KERN_ALERT"Inside the %s function",__FUNCTION__);
+}
+module_exit(ex03_simple_module_exit);
+
